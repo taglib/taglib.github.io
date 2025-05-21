9bb94b06f992201cd7bc4fe6cc999b7e500097a3
Urs Fleisch <ufleisch@users.sourceforge.net>
2025-05-21 21:10:26 +0200

    Update API docs for version 2.1
    Generated using ./_generate_docs /path/to/src/taglib

--- api/menudata.js
+++ api/menudata.js
@@ -143,7 +143,7 @@ var menudata={children:[
 {text:"v",url:"functions_eval_v.html#index_v"},
 {text:"w",url:"functions_eval_w.html#index_w"},
 {text:"x",url:"functions_eval_x.html#index_x"}]},
-{text:"Related Functions",url:"functions_rela.html"}]}]},
+{text:"Related Symbols",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
